49a7b11e090ba788880dd30ae19a246040ab27e7
tanya2407 <tanyagarg630@gmail.com>
2020-06-11 19:54:04 +0530

    added comment

--- Arrays/NewYearChaos.cpp
+++ Arrays/NewYearChaos.cpp
@@ -1,4 +1,7 @@
 /*
+HackerRank practice qustions -> interview kit-> arrays
+*/
+/*
 It's New Year's Day and everyone's in line for the Wonderland rollercoaster ride! There are a number of people queued up,
  and each person wears a sticker indicating their initial position in the queue. Initial positions increment by 1 from 1 at the
  front of the line to n at the back. Any person in the queue can bribe the person directly in front of them to swap positions.
